65e51d286c8b5ace3e4ccc11c6cd4abe4ef11d93
Shrey <ptl.shreys@gmail.com>
2025-08-09 03:45:39 +0200

    Here I learned how to use a library called PrettyTables and it was simple and very informational.
    The most important thing i learned here was the creation of an object and usage of the associated methods in practice and how easy Python really is.

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from turtle import Turtle,Screen
 
 # Turner is the object and Turtle is the class from the module turtle
-turner = Turtle()
-print(turner)
-turner.shape("turtle")
-turner.color("DarkSeaGreen2")
-turner.forward(100)
+# turner = Turtle()
+# print(turner)
+# turner.shape("turtle")
+# turner.color("DarkSeaGreen2")
+# turner.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# Now I will Learn how to use the many available external Python libraries.
+# Like the ones from the Python Package Index or PyPI for short.
 
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+table.align = "l"
+print(table)
