96182cc106194a7728c9f2e666315bc9412bb80c
Shrey <ptl.shreys@gmail.com>
2025-08-09 03:26:17 +0200

    Here I learned how to use a library to create an object and even revisited a little bit of the turtle library.
    The most important thing i learned here was the creation of an object and usage of the associated methods.

---
+++ main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle,Screen
+
+# Turner is the object and Turtle is the class from the module turtle
+turner = Turtle()
+print(turner)
+turner.shape("turtle")
+turner.color("DarkSeaGreen2")
+turner.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
